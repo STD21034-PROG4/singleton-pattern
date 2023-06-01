55a79b230e4518bdfe265d953266984ff4dea76f
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2023-06-01 11:40:25 +0300

    chore: add gitignore and refactor test

--- src/test/java/ClassSingletonTest.java
+++ src/test/java/ClassSingletonTest.java
@@ -5,12 +5,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ClassSingletonTest {
   private static final String EXPECTED_DESCRIPTION = "Initial description";
+  private static final String NEW_DESCRIPTION = "New description";
 
   @Test
   void read_description_ok(){
-    String actual = ClassSingleton.getInstance().getDescription();
+    ClassSingleton actual1 = ClassSingleton.getInstance();
+    assertEquals(EXPECTED_DESCRIPTION, actual1.getDescription());
 
-    assertEquals(EXPECTED_DESCRIPTION, actual);
+    ClassSingleton actual2 = ClassSingleton.getInstance();
+    actual1.setDescription(NEW_DESCRIPTION);
+
+    assertEquals(NEW_DESCRIPTION, actual2.getDescription());
   }
 
 }
--- src/test/java/ClassUniqueSingletonTest.java
+++ src/test/java/ClassUniqueSingletonTest.java
@@ -5,12 +5,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ClassUniqueSingletonTest {
   private static final int EXPECTED_CLICK = 0;
+  private static final int NEW_CLICK = 1;
 
   @Test
   void read_click_ok() {
-    int actual = ClassUniqueInstance.INSTANCE.getInstance().getClick();
+    ClassUniqueInstance actual1 = ClassUniqueInstance.INSTANCE.getInstance();
+    assertEquals(EXPECTED_CLICK, actual1.getClick());
 
-    assertEquals(EXPECTED_CLICK, actual);
+    ClassUniqueInstance actual2 = ClassUniqueInstance.INSTANCE.getInstance();
+    actual1.setClick(1);
+
+    assertEquals(NEW_CLICK, actual2.getClick());
   }
 
 }
